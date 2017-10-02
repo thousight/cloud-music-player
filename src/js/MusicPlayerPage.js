bdbbd7dfa9f2173ff6af6df55396400eab674cc9
Pengyu Chen <chen2087@purdue.edu>
2017-10-01 23:25:28 -0400

    create the UI for music play page

--- src/js/MusicPlayerPage.js
+++ src/js/MusicPlayerPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { Row, Col } from 'react-bootstrap';
 import '../css/MusicPlayerPage.css';
 
 class MusicPlayerPage extends Component {
@@ -8,7 +8,27 @@ class MusicPlayerPage extends Component {
     return (
       <div className="music-player-page">
         <h1>Music Player Page</h1>
+        <div className ="musicc-playlist">
+            <Row>
+                <Col md={6}>
+                    <h1>Music List</h1>
+                </Col>
+
+                <Col md={6}>
+                    <div className = "music-info">
+                      <h1>Muisc Information</h1>
+                      <Row>
+                        <h1>Play Bar</h1>
+                      </Row>
+                    </div>
+                </Col>
+            </Row>
+          </div>
       </div>
+
+
+
+
     );
   }
 }
