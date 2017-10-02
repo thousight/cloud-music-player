64795d3d17fe06d9f8421d14743956502388237d
Shiroin <zhong76@purdue.edu>
2017-10-01 22:51:13 -0400

    Basic UI and Buttons for import page

--- src/js/ImportPage.js
+++ src/js/ImportPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
+import { Row, Col } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
-
 class ImportPage extends Component {
 
   selectAllButtonOnClick() {
@@ -10,9 +10,46 @@ class ImportPage extends Component {
   render() {
     return (
       <div className="import-page">
-        <h1>Import Page</h1>
-        <Button className="select-all-button" onClick={this.selectAllButtonOnClick.bind(this)}>Select All</Button>
+            <Col className="navigating" md={5} mdOffset={2}>
+              <Row className="navigating-header">
+                <Col md={3}>
+                  <Button className="back-to-parent-button" onClick={this.selectAllButtonOnClick.bind(this)}></Button>
+                </Col>
+                <Col md={5}>
+                  <h1 className="header">
+                    User's Music Folder
+                  </h1>
+                </Col>
+              </Row>
+              <Row className="navigating-content">
+                <Col >
+                  <Row>
+                    <Button className="select-all-button" onClick={this.selectAllButtonOnClick.bind(this)}>Select All</Button>
+                  </Row>
+                </Col>
+
+              </Row>
+            </Col>
+            <Col className="selecting" md={2} mdOffset={1}>
+              <Row>
+                <h1>
+                  Selected Music File
+                </h1>
+              </Row>
+              <Row>
+                <Col md={6}>
+                  <Button className="clear-button" onClick={this.selectAllButtonOnClick.bind(this)}>X    Clear</Button>
+                </Col>
+                <Col md={6}>
+                  <Button className="submit-button" onClick={this.selectAllButtonOnClick.bind(this)}>√   Submit</Button>
+                </Col>
+              </Row>
+
+
+            </Col>
+
       </div>
+
     );
   }
 }
