a7c46b19050e49e15582e9f7b44dfb628b6a0554
Unknown <chen2087@purdue.edu>
2017-10-12 17:13:22 -0400

    save changes

--- src/js/App.js
+++ src/js/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import { Switch, Route } from 'react-router-dom';
+import { withRouter } from 'react-router';
 
 import LoginPage from './LoginPage';
 import ImportPage from './ImportPage';
@@ -10,14 +11,10 @@ import { userLogin } from './redux/actions';
 
 class App extends Component {
 
-  state = {
-    isLoggedIn: false
-  }
-
   componentWillMount() {
     require('google-client-api')().then(gapi => {
       gapi.load('auth2:client', () => {
-        window.gapi.auth2.init({
+        gapi.auth2.init({
           client_id: '864033579706-cig1gmgglj5q8ko8uocv8kkbpb4g46tv.apps.googleusercontent.com',
           cookiepolicy: 'single_host_origin',
           api_key: 'AIzaSyDe81MXEotfiSTyJA_7EOvbtWhFKr93Y28',
@@ -38,6 +35,7 @@ class App extends Component {
   }
 
   render() {
+
     return (
       <div className="App">
         {this.props.user.name ? <Navbar /> : <div />}
@@ -50,29 +48,11 @@ class App extends Component {
     );
   }
 }
-/*
-// 1. Create the button
-var button = document.createElement("button");
-button.innerHTML = "select all";
-// 2. Append somewhere
-var body = document.getElementsByTagName("body")[0];
-body.appendChild(button);
-// 3. Add event handler
-button.addEventListener ("click", function() {
-  alert("did something");
-});
-*/
-
-
-const mapDispatchToProps = dispatch => {
-	return bindActionCreators({
-		userLogin
-	}, dispatch);
-}
 
 const mapStateToProps = state => {
-	return {
-		user: state.user
-	}
+  return {
+    user: state.user
+  }
 }
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+export default connect(mapStateToProps)(withRouter(App));
