e2a13cdecfdb1b523fbf1943301955b6822f299d
Mark Wen <markwenguojie94@gmail.com>
2017-10-23 15:47:57 -0400

    Quick fix

--- src/js/MusicPlayerPage.js
+++ src/js/MusicPlayerPage.js
@@ -12,7 +12,6 @@ import play from '../img/play_arrow.svg';
 import volume from '../img/volume.svg';
 import cover from '../img/kris.jpg';
 
-import { Button } from 'react-bootstrap';
 class MusicPlayerPage extends Component {
 
   state = {
@@ -23,7 +22,6 @@ class MusicPlayerPage extends Component {
     // Sidebar content stuff
     const sidebarContent = (<div style={{ width: '300px' }}>
         <SidebarContent />
-        <Button className="roundbutton" onClick={this.backButtonOnClick.bind(this)}>←</Button>
       </div>);
 
     return (
