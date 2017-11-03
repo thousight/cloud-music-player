0c407bd22ddcbbf9a74732700c5882729bd52e80
Mark Wen <markwenguojie94@gmail.com>
2017-11-03 11:31:25 -0400

    Fixed album art display issue

--- src/js/MusicPlayerPage.js
+++ src/js/MusicPlayerPage.js
@@ -50,29 +50,40 @@ class MusicPlayerPage extends Component {
   }
 
   getMusicMetadata() {
-      if (this.props.packages.gapi && this.props.user.currentlyPlayingMusicId) {
-        let xhr = new XMLHttpRequest();
-        xhr.open('GET', `https://www.googleapis.com/drive/v3/files/${this.props.user.currentlyPlayingMusicId}?alt=media`, true);
-        xhr.setRequestHeader('Authorization', `Bearer ${this.props.packages.gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse().access_token}`)
-        xhr.withCredentials = true;
-        xhr.responseType = 'blob';
-        xhr.onload = () => {
-          jsmediatags.read(xhr.response, {
-            onSuccess: tag => {
-              this.setState({
-                title: tag.tags.title,
-                singer: tag.tags.artist,
-                album: tag.tags.album,
-                cover: tag.tags.picture ? tag.tags.picture.data.toString('base64') : null
-              });
-            },
-            onError: error => {
-              console.log(error);
-            }
-          })
-        }
-        xhr.send();
+    if (this.props.packages.gapi && this.props.user.currentlyPlayingMusicId) {
+      let xhr = new XMLHttpRequest();
+      xhr.open('GET', `https://www.googleapis.com/drive/v3/files/${this.props.user.currentlyPlayingMusicId}?alt=media`, true);
+      xhr.setRequestHeader('Authorization', `Bearer ${this.props.packages.gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse().access_token}`)
+      xhr.withCredentials = true;
+      xhr.responseType = 'blob';
+      xhr.onload = () => {
+        jsmediatags.read(xhr.response, {
+          onSuccess: tag => {
+            console.log(tag.tags);
+            this.setState({
+              title: tag.tags.title,
+              singer: tag.tags.artist,
+              album: tag.tags.album,
+              cover: tag.tags.picture ? `data:${tag.tags.picture.format};base64,${this.arrayBufferToBase64(tag.tags.picture.data)}`: null
+            });
+          },
+          onError: error => {
+            console.log(error);
+          }
+        })
       }
+      xhr.send();
+    }
+  }
+
+  arrayBufferToBase64(buffer) {
+    let binary = '';
+    let bytes = new Uint8Array( buffer );
+    let len = bytes.byteLength;
+    for (let i = 0; i < len; i++) {
+      binary += String.fromCharCode( bytes[ i ] );
+    }
+    return window.btoa( binary );
   }
 
   render() {
@@ -91,7 +102,10 @@ class MusicPlayerPage extends Component {
         open={this.props.settings.isSidebarOpen}
         docked={this.state.sidebarDocked} >
         <div className="player-page">
-          <img className="player-page-cover" alt="cover" src={this.state.cover ? this.state.cover : singleNodeIcon} />
+          <img className="player-page-cover"
+            alt="cover"
+            src={this.state.cover ? this.state.cover : singleNodeIcon}
+            style={{padding: this.state.cover ? '0' : '10px'}} />
           <h3>{this.state.title}</h3>
           <h5>{this.state.singer}</h5>
           <h5>{this.state.album}</h5>
